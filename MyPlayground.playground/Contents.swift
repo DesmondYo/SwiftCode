f9e4fc30c649dd7c779b236ac9325be8d5a38b27
Desmond Young <Desmond.Young25@yahoo.com>
2020-04-10 12:08:19 -0700

    Created (If), (Else If), (Else), statements

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,14 +1,16 @@
 import UIKit
 
-var str = "Hello, playground"
+let a = 25
+let b = 10
+let c = 1
 
-var firstName = "Dez"
-print (firstName)
-
-var stockPrice = 100
-print(stockPrice)
-
-stockPrice = 50
-print (stockPrice)
-
-let lastName = "Young"
+if (a <= 10 || b > 5) || c != 1  {
+    print("branch 1")
+}else if a < 15 {
+    print("branch 2")
+}else if a > 30 {
+    print("branch 3")
+}else{
+    print("catch all")
+    
+}
