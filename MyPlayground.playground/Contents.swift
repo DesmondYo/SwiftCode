96b15427d8f829a675be75f796cc937cc7e95897
Desmond Young <Desmond.Young25@yahoo.com>
2020-04-16 12:14:59 -0700

    Learned about return values , also how to input parameters and how to specify them.

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,41 +2,15 @@ import UIKit
 
     var str = "Hello, playground"
 
-    var counter = -5
-
-    while counter > 0 {
-
-        print("Hello from while loop ")
-        counter -= 1;
-}
-
-    var counter2 = -5
-
-    repeat{
-        print("Hello from repeat while loop")
-        counter2 -= 1;
-    } while counter2 > 0
-
-
-    func addTwoNumbers() {
+func addTwoNumbers(using number1: Int, and number2: Int)-> Int {
     
-    let a = 1
-    let b = 2
+    let a = number1
+    let b = number2
     let c = a + b
-    
-    print (c)
-    
-}
-    addTwoNumbers()
+   
+    return c
 
-    func subtractTwoNumbers() {
-    
-    let d = 10
-    let e = 5
-    let f = d - e
-    
-    print (f)
-        
 }
 
-    subtractTwoNumbers()
+let sum = addTwoNumbers(using: 10, and: 10)
+print(sum)
