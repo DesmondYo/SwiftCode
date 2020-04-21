0cec05330d3c8e4cb9397b343060359c1aeb996c
Desmond Young <Desmond.Young25@yahoo.com>
2020-04-21 11:53:14 -0700

    Designated & Convenience Initializers

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,69 +1,33 @@
 import UIKit
 
-
 class Person {
-    var name = ""
+
+    var name:String
+    var netWorth:Int?
+    var gender:String!
     
+    //Designated initializer because it makes sure that all properties are initialized
     init() {
-        
+        name = "None"
     }
     
-    init(_ name:String) {
-        self.name = name
+    convenience init(_ gender:String, netWorth:Int) {
+        //Call the designated initializer to ensure that the object is ready to go
+        self.init()
+        
+        //Set any other properties or custom code to initialize for this scenario
+        self.gender = gender
+        self.netWorth = netWorth
     }
 }
+// Creating a new person obj
+let a = Person()
 
-class Employee:Person {
-    
-    var salary = 0
-    var role = ""
-    
-    override init (_ name:String) {
-        
-        //This is calling the init (_ name:String) function of the Person class
-        super.init(name)
-        
-        //Additional init code
-        self.role = "Anaylst"
-    }
-    
-    func doWork () {
-        print ("Hello my name is " + name + " and I am doing work")
-        salary += 1
-    }
-}
+//Creating a new rich person obj
+let b = Person("Male", netWorth: 50000)
+let c = Person("Female", netWorth: 100000)
 
-class Manager: Employee {
-    
-    var teamSize = 0
-    var bonus: Int {
-        
-        // This is  a computed property
-        //When it's acccessed, the code in here will run
-        //Then we'll return the value
-        return teamSize * 1000
-    }
-    
-    init(_ name:String, _ team:Int) {
-        
-        //This calls the init in the employee class
-        super.init(name)
-        
-        //Additonal init work
-        teamSize = team
-    }
-    
-    override func doWork() {
-        super.doWork()
-        
-        print("I am managing people")
-        salary += 2
-    }
-    
-    func firePeople () {
-        print("I am firing people")
-    }
-}
 
-let m = Manager("Kate", 11)
-print(m.bonus)
+
+
+
